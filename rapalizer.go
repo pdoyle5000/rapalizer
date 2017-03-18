59dce9a759e8b1b62b5fb1251a2e928e274c2c21
pdoyle5000 <patrick.r.doyle@maine.edu>
2017-03-18 15:38:22 -0400

    initial commit

---
+++ rapalizer.go
@@ -0,0 +1,108 @@
+package rapalizer
+
+import (
+	"fmt"
+	"strings"
+)
+
+const NUM_WORDS_TO_FIND_RHYMES int = 20
+const MIN_CHARS_IN_ELIGIBLE_WORD int = 2
+
+type Rapalizer struct {
+	Lyrics 	[]string 		`json:"lyrics"`
+	Score 	int 			`json:"score"`
+	Artist 	string 			`json:"artist"`
+	Title 	string 			`json:"title"`
+	Pairs 	[]LyricPairs 	`json:"pairs"`
+}
+
+type LyricPairs struct {
+	First 	string
+	Second 	string
+}
+//TODO:  ToJson function
+
+func (rap *Rapalizer) LoadStringIntoWordArray(lyrics string) {
+	cleansedString := strings.Replace(lyrics, ")", "", -1)
+	cleansedString = strings.Replace(cleansedString, "(", "", -1)
+	cleansedString = strings.Replace(cleansedString, "\n", " ", -1)
+	cleansedString = strings.Replace(cleansedString, "\r", " ", -1)
+	importedLyrics := strings.Split(cleansedString, " ")
+	if len(importedLyrics) > 0 {
+		for _, word := range importedLyrics {
+			rap.Lyrics = append(rap.Lyrics, word)
+		}
+	}
+}
+
+func (rap *Rapalizer) CalculateScore() {
+	wordsToSkip := []string{"is", "but", "if", "in", "as", "a", "i", 
+	"the", "of", "or", "an", "and", "you", "me", " ", ""}
+	if len(rap.Lyrics) == 0 {
+		rap.Score = -9999
+	} else {
+		rap.CalculateRapScore(rap.Lyrics, wordsToSkip)
+		fmt.Println("Final Score:", rap.Score)
+	}
+}
+
+func (rap *Rapalizer) CalculateRapScore(words []string, skipWords []string) {
+	for i, word := range words {
+		if !IsWordInSlice(word, skipWords) {
+			if (len(words) - i) < NUM_WORDS_TO_FIND_RHYMES {
+				rap.CompareSuffixes(NormalizeWord(word), words[i:], skipWords)
+			} else {
+				rap.CompareSuffixes(NormalizeWord(word), words[i:i+NUM_WORDS_TO_FIND_RHYMES], skipWords)
+			}
+		}
+	}
+}
+
+func (rap *Rapalizer) CompareSuffixes(target string, words []string, skipWords []string) {
+	for i := 1; i < len(words); i++ {
+		thisword := NormalizeWord(words[i])
+		if !IsWordInSlice(thisword, skipWords) && len(thisword) > 1 {
+			rap.CompareSuffixPair(target, thisword)
+		}
+	}
+}
+
+func (rap *Rapalizer) CompareSuffixPair(firstWord string, compareWord string) {
+	firstSuffix := firstWord[len(firstWord)-MIN_CHARS_IN_ELIGIBLE_WORD:]
+	compareSuffix := compareWord[len(compareWord)-MIN_CHARS_IN_ELIGIBLE_WORD:]
+	if firstSuffix == compareSuffix {
+		if firstWord == compareWord {
+			rap.Score--
+			fmt.Println("Rem One:", firstWord, compareWord)
+		} else {
+			fmt.Println("Add One:", firstWord, compareWord)
+			rap.Pairs = append(rap.Pairs, LyricPairs{firstWord, compareWord})
+			rap.Score++
+		}
+	}
+}
+
+func NormalizeWord(word string) string {
+	return StripPunctuation(strings.ToLower(word))
+}
+
+func StripPunctuation(word string) string {
+	if strings.ContainsAny(word, ".|!|,|?|:|;|)") {
+		if strings.ContainsAny(word, "(") {
+			return word[1:len(word)-1]
+		} else {
+			return word[:len(word)-1]
+		}
+	}
+	return word
+}
+
+func IsWordInSlice(entry string, wordslice []string) bool {
+	for _, word := range wordslice {
+
+		if NormalizeWord(entry) == NormalizeWord(word) {
+			return true
+		}
+	}
+	return false
+}
---
+++ rapalizer_test.go
@@ -0,0 +1,170 @@
+package rapalizer
+import (
+	"testing"
+	"fmt"
+)
+
+const singleRhyme string = "You missed two classes and no homework.  But your teacher preaches class like you're some kind of jerk!"
+
+const fullSong string = "Kick it! " +
+	"You wake up late for school man you don't wanna go! " +
+	"You ask you mom, Please? but she still says, No! " +
+	"You missed two classes and no homework " +
+	"But your teacher preaches class like you're some kind of jerk " +
+	"You gotta fight for your right to party " +
+	"You pops caught you smoking and he said, No way! " +
+	"That hypocrite smokes two packs a day " +
+	"Man, living at home is such a drag " +
+	"Now your mom threw away your best porno mag (Busted!) " +
+	"Dont step out of this house if thats the clothes youre gonna wear " +
+	"Ill kick you out of my home if you dont cut that hair " +
+	"Your mom busted in and said, Whats that noise? " +
+	"Aw, mom youre just jealous its the Beastie Boys!"
+
+func TestNullString(t *testing.T) {
+	var rap Rapalizer
+	rap.CalculateScore()
+	if rap.Score != -9999 {
+		t.Error("Empty String did not return negative score: ",  rap.Score)
+	}
+}
+
+func TestSingleWordString(t *testing.T) {
+	var rap Rapalizer
+	rap.LoadStringIntoWordArray("Word.")
+	rap.CalculateScore()
+	if rap.Score != 0 {
+		fmt.Println("Score: ", rap.Score)
+		t.Error("Single word cannot rhyme and should return a 0 score.")
+	}
+}
+
+func TestReplicateWord(t *testing.T) {
+	var rap Rapalizer
+	rap.LoadStringIntoWordArray("Word word.")
+	rap.CalculateScore()
+	if rap.Score != -1 {
+		fmt.Println("Score: ", rap.Score)
+		t.Error("double word.  should return -1 score.")
+	}
+}
+
+func TestSingleRhyme(t *testing.T) {
+	var rap Rapalizer
+	rap.LoadStringIntoWordArray("Word Turd.")
+	rap.CalculateScore()
+	if rap.Score != 1 {
+		fmt.Println("Score: ", rap.Score)
+		t.Error("single rhyme.  should return 1 score.")
+	}
+}
+
+func TestSingleNoRhyme(t *testing.T) {
+	var rap Rapalizer
+	rap.LoadStringIntoWordArray("Word Nope.")
+	rap.CalculateScore()
+	if rap.Score != 0 {
+		fmt.Println("Score: ", rap.Score)
+		t.Error("no rhyme.  should return 0 score.")
+	}
+}
+
+func TestSingleRhymeWithSkipWords(t *testing.T) {
+	var rap Rapalizer
+	rap.LoadStringIntoWordArray("Word and a Turd.")
+	rap.CalculateScore()
+	if rap.Score != 1 {
+		fmt.Println("Score: ", rap.Score)
+		t.Error("single rhyme with skip words.  should return 1 score.")
+	}
+}
+
+func TestTripleRhymeWithSkipWords(t *testing.T) {
+	var rap Rapalizer
+	rap.LoadStringIntoWordArray("Word and a Turd and a Bird.")
+	rap.CalculateScore()
+	if rap.Score != 3 {
+		fmt.Println("Score: ", rap.Score)
+		t.Error("single rhyme with skip words.  should return 3 score.")
+	}
+}
+
+func TestRealSingleRhymeLyricUnderTwentyWords(t *testing.T) {
+	var rap Rapalizer
+	rap.LoadStringIntoWordArray(singleRhyme)
+	rap.CalculateScore()
+	if rap.Score != 2 {
+		fmt.Println("Score: ", rap.Score)
+		t.Error("single rhyme with skip words.  should return 1 score.")
+	}
+}
+
+func TestRealSingleRhymeLyricOverTwentyWords(t *testing.T) {
+	var rap Rapalizer
+	rap.LoadStringIntoWordArray(fullSong)
+	rap.CalculateScore()
+	if rap.Score != 6 {
+		fmt.Println("Score: ", rap.Score)
+		t.Error("full rhyme with skip words.  should return SOME score.")
+	}
+}
+
+func TestisWordInSlice(t *testing.T) {
+	warray := []string{ "I", "Will", "Take", "A", "Giant", "Dump"}
+	if IsWordInSlice("Will", warray) != true {
+		t.Error("Word is in slice but returning false.")
+	}
+	if IsWordInSlice("Farts", warray) != false {
+		t.Error("Word is not in slice but returning true.")
+	}
+}
+
+func TestStripPunctuation(t *testing.T) {
+	if "Farting" != StripPunctuation("Farting!") {
+		t.Error("Ending punctuation on a word is not stripped.")
+	}
+	if "Farting" != StripPunctuation("(Farting!") {
+		t.Error("Starting punctuation on a word is not stripped.")
+	}
+	if "Farting" != StripPunctuation("(Farting?") {
+		t.Error("Double sided punctuation on a word is not stripped.")
+	}
+	if "Farting" != StripPunctuation("Farting") {
+		t.Error("Clean word is not properly passing through logic.")
+	}
+}
+
+func TestNormalizingWords(t *testing.T) {
+	if "farting" != NormalizeWord("(FaRtiNg?") {
+		t.Error("Word Normalization failed.")
+	}
+}
+
+func TestLoadingStringIntoWordArray(t *testing.T) {
+	var rap Rapalizer
+	rap.LoadStringIntoWordArray("")
+	if len(rap.Lyrics) != 1 {
+		t.Error("Lyric-Word array should be empty. Size: ", len(rap.Lyrics))
+	}
+	var rap1 Rapalizer
+	rap1.LoadStringIntoWordArray("word")
+	if len(rap1.Lyrics) != 1 || rap1.Lyrics[0] != "word" {
+		t.Error("Should only have one word in array: ", rap1.Lyrics[0])
+	}
+	var rap2 Rapalizer
+	rap2.LoadStringIntoWordArray("This is a complete Sentence.")
+	ideal := []string{"This", "is", "a", "complete", "Sentence."}
+	if len(rap2.Lyrics) != 5  {
+		t.Error("Sentence loaded incorrectly: ", rap2.Lyrics)
+	}
+	for i := range rap2.Lyrics {
+		if rap2.Lyrics[i] != ideal[i] {
+			t.Error("Slice is not equal to ideal slice.", rap2.Lyrics)
+		}
+	}
+	var rap3 Rapalizer
+	rap3.LoadStringIntoWordArray(singleRhyme)
+	if len(rap3.Lyrics) != 19 {
+		t.Error("Beastie Boys Lyric did not properly load: ", len(rap3.Lyrics), rap3.Lyrics)
+	}
+}
