86487c5df1e8441a4733bc5c8af0e43583a991bf
pdoyle5000 <patrick.r.doyle@maine.edu>
2017-03-19 18:37:51 -0400

    Added execute.go

---
+++ execute.go
@@ -0,0 +1,6 @@
+// main application
+// start the server
+// assign elasticsearch address and endpoints
+// act as back end for webcomponents
+// get input, verify artist/title combo is not in DB already
+// give to rapalizer, get results, give to front end and place in DB.
